551680ac7b269ea5d4e621ccb86cfaf3569131fc
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2020-04-22 23:27:23 +0800

    update: update style

--- lib/components/layout.jsx
+++ lib/components/layout.jsx
@@ -2,31 +2,13 @@ import React, { useEffect, useMemo, useState } from 'react'
 import Head from 'next/head'
 import Profile from './profile'
 import Contacts from './contacts'
-import { useRouter } from 'next/router'
-import { msToString } from '../data-transform'
-import { Spacer, Text, useTheme, Image } from '@zeit-ui/react'
+import Title from './title'
+import { Spacer } from '@zeit-ui/react'
 import BLOG from '../../blog.config'
 
-const getDate = date => {
-  const d = new Date(date)
-  if (`${d}` === 'Invalid Date') return ''
-  const time = Date.now() - new Date(date).getTime()
-  return `${d.toLocaleString()} - ${msToString(time)}`
-}
-
 const Layout = ({ children, meta = {} }) => {
-  const theme = useTheme()
-  const { asPath } = useRouter()
   const [showAfterRender, setShowAfterRender] = useState(false)
-  
   const inDetailPage = useMemo(() => meta && meta.title, [])
-  const date = useMemo(() => getDate((meta || {}).date), [])
-  const url = useMemo(() => {
-    const suffix = BLOG.cn ? ' 阅读' : ' views'
-    return `https://views.show/svg?key=${asPath}&fill=666666&suffix=${encodeURI(suffix)}&size=13`
-  }, [asPath])
-  const showViews = useMemo(() => BLOG.enableViews, [])
-  
   useEffect(() => setShowAfterRender(true), [])
   
   if (!showAfterRender) return null
@@ -41,22 +23,13 @@ const Layout = ({ children, meta = {} }) => {
         {meta.image && <meta property="twitter:image" content={meta.image} />}
       </Head>
       <div className="container">
-        {inDetailPage && <Spacer />}
+        <Spacer />
         <Profile />
-        {inDetailPage && <Spacer y={1} />}
-        {inDetailPage && <Text h1>{meta.title}</Text>}
-        {inDetailPage && (
-          <div className="date-box">
-            <Text p className="date">{date}</Text>
-            {showViews && <Image width={100} height={24} src={url} />}
-          </div>
-        )}
-        {inDetailPage && <Spacer y={1} />}
+        {inDetailPage && <Title title={meta.title} date={meta.date} />}
         {children}
         <Spacer y={5} />
         <Contacts />
       </div>
-      
 
       <style jsx>{`
         section {
@@ -92,55 +65,10 @@ const Layout = ({ children, meta = {} }) => {
           font-size: 1.2rem;
         }
         
-        .date-box {
-          display: flex;
-          width: fit-content;
-          align-items: center;
-          height: 30px;
-          margin: -.5rem 0 0 0;
-          position: relative;
-        }
-        
-        .date-box>:global(.date) {
-          color: ${theme.palette.accents_5};
-          font-size: .85rem;
-        }
-        
-        .date-box :global(.image) {
-          position: absolute;
-          left: 100%;
-          top: 50%;
-          transform: translateY(-50%);
-          display: inline-flex;
-          align-items: center;
-          margin: 0 0 0 10px;
-        }
-        
-        .date-box :global(img) {
-          object-fit: unset;
-        }
-        
         @media only screen and (max-width: 767px) {
           .container {
-            max-width: 91vw;
+            max-width: 88vw;
             min-height: 100vh;
-          }
-          
-          .container :global(h1) {
-            text-align: center;
-          }
-          
-          .container>:global(.date) {
-            text-align: center;
-          }
-          
-          .date-box {
-            justify-content: center;
-            margin: 0 auto;
-          }
-          
-          .date-box :global(.image) {
-            display: none;
           }
         }
       `}</style>

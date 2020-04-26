1f33c7e5bea1033094ab072ddbb08c44f797bf0b
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2020-04-26 16:06:51 +0800

    Revert "Revert "update: update style""
    This reverts commit 9161aa5e60aa4793182d477aab8fe429b9674012.

--- lib/components/layout.jsx
+++ lib/components/layout.jsx
@@ -4,31 +4,46 @@ import Profile from './profile'
 import Contacts from './contacts'
 import Title from './title'
 import { Spacer } from '@zeit-ui/react'
-import BLOG from '../../blog.config'
+import { Configs } from '../utils'
+
+const LayoutHeader = ({ meta }) => (
+  <Head>
+    {meta.title && <title>{meta.title} - {Configs.title}</title>}
+    {meta.description && <meta name="description" content={meta.description} />}
+    {meta.description && <meta property="og:description" content={meta.description} />}
+    {meta.title && <meta property="og:title" content={meta.title} />}
+    {meta.image && <meta property="og:image" content={meta.image} />}
+    {meta.image && <meta property="twitter:image" content={meta.image} />}
+  </Head>
+)
 
 const Layout = ({ children, meta = {} }) => {
   const [showAfterRender, setShowAfterRender] = useState(false)
   const inDetailPage = useMemo(() => meta && meta.title, [])
   useEffect(() => setShowAfterRender(true), [])
   
-  if (!showAfterRender) return null
+  if (!showAfterRender) return (
+    <div className="article-content">
+      <LayoutHeader meta={meta} />
+      {children}
+      <style jsx>{`
+        .article-content {
+          opacity: 0;
+          display: none;
+        }
+      `}</style>
+    </div>
+  )
   return (
     <section>
-      <Head>
-        {meta.title && <title>{meta.title} - {BLOG.title}</title>}
-        {meta.description && <meta name="description" content={meta.description} />}
-        {meta.description && <meta property="og:description" content={meta.description} />}
-        {meta.title && <meta property="og:title" content={meta.title} />}
-        {meta.image && <meta property="og:image" content={meta.image} />}
-        {meta.image && <meta property="twitter:image" content={meta.image} />}
-      </Head>
+      <LayoutHeader meta={meta} />
       <div className="container">
         <Spacer />
         <Profile />
         {inDetailPage && <Title title={meta.title} date={meta.date} />}
         {children}
         <Spacer y={5} />
-        <Contacts />
+        <Contacts isDetailPage={inDetailPage} />
       </div>
 
       <style jsx>{`
@@ -42,11 +57,10 @@ const Layout = ({ children, meta = {} }) => {
         
         .container {
           width: 100%;
-          max-width: 750px;
+          max-width: ${Configs.layouts.pageWidth};
           display: flex;
           flex-direction: column;
           justify-content: flex-start;
-          text-spacing: none;
         }
         
         .container :global(h1) {
@@ -67,7 +81,7 @@ const Layout = ({ children, meta = {} }) => {
         
         @media only screen and (max-width: 767px) {
           .container {
-            max-width: 88vw;
+            max-width: ${Configs.layouts.pageWidthMobile};
             min-height: 100vh;
           }
         }
